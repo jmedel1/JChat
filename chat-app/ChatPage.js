156e00680d04bb622e374ac17243de7bf5ed330d
jael <jael.medel30@gmail.com>
2023-06-28 11:40:20 -0500

    display list of messages

--- chat-app/ChatPage.js
+++ chat-app/ChatPage.js
@@ -1,12 +1,39 @@
-import React from 'react';
-import ChatRoom from './ChatRoom';
+import React, { useEffect, useState } from 'react';
 
 function ChatPage() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    // Simulating fetching messages from a server or database
+    // Replace this with your own logic to fetch messages
+    const fetchMessages = async () => {
+      // Simulating an API call delay
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+
+      // Dummy data for example
+      const dummyMessages = [
+        { id: 1, text: 'Hello', user: 'User1' },
+        { id: 2, text: 'How are you?', user: 'User2' },
+        { id: 3, text: 'I am good. How about you?', user: 'User1' },
+      ];
+
+      setMessages(dummyMessages);
+    };
+
+    fetchMessages();
+  }, []);
+
   return (
     <div>
       <h1>Chat Page</h1>
-      {/* Additional components or layout for the chat page */}
-      <ChatRoom />
+      <ul>
+        {messages.map((message) => (
+          <li key={message.id}>
+            <strong>{message.user}: </strong>
+            {message.text}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
