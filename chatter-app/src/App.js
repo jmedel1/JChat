212d94fccf9e972e3ea322bc7eb9ac10319d4071
jael <jael.medel30@gmail.com>
2023-06-28 12:59:12 -0500

    updating app

---
+++ chatter-app/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import ChatPage from './ChatPage';
+import SettingsPage from './SettingsPage';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Chat</Link>
+            </li>
+            <li>
+              <Link to="/settings">Settings</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route exact path="/">
+            <ChatPage />
+          </Route>
+          <Route path="/settings">
+            <SettingsPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
