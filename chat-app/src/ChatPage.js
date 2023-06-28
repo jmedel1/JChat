57c701e97f7ee4d2ccc22994e91ebdc3bd2362b0
jael <jael.medel30@gmail.com>
2023-06-28 12:16:50 -0500

    fetch messages

--- chat-app/src/ChatPage.js
+++ chat-app/src/ChatPage.js
@@ -2,72 +2,27 @@ import React, { useState, useEffect } from 'react';
 
 function ChatPage() {
   const [messages, setMessages] = useState([]);
-  const [newMessage, setNewMessage] = useState('');
 
   useEffect(() => {
-    // Fetch messages from the backend or an external API
-    // You can replace this with your own logic
-    const fetchMessages = async () => {
-      try {
-        // Simulating a fetch request delay with setTimeout
-        await new Promise((resolve) => setTimeout(resolve, 1000));
-
-        // Mock data for demonstration purposes
-        const response = [
-          { id: 1, sender: 'John', content: 'Hello' },
-          { id: 2, sender: 'Jane', content: 'Hi there' },
-        ];
-
-        setMessages(response);
-      } catch (error) {
-        console.log('Error fetching messages:', error);
-      }
-    };
-
     fetchMessages();
   }, []);
 
-  const handleNewMessageChange = (e) => {
-    setNewMessage(e.target.value);
-  };
-
-  const handleSendMessage = (e) => {
-    e.preventDefault();
-
-    // Create a new message object
-    const newMessageObj = {
-      id: messages.length + 1,
-      sender: 'You', // You can replace this with the user's display name or any other identifier
-      content: newMessage,
-    };
-
-    // Add the new message to the existing messages array
-    setMessages([...messages, newMessageObj]);
-
-    // Clear the input field
-    setNewMessage('');
-  };
+  async function fetchMessages() {
+    const response = await fetch('http://localhost:3001/messages');
+    const data = await response.json();
+    setMessages(data);
+  }
 
   return (
-    <div>
-      <h1>Chat Page</h1>
-      <div className="message-list">
-        {messages.map((message) => (
-          <div key={message.id} className="message">
-            <strong>{message.sender}: </strong>
-            <span>{message.content}</span>
-          </div>
+    <div className="ChatPage">
+      <h1>Chat Room</h1>
+      <ul>
+        {messages.map(message => (
+          <li key={message.id}>
+            <strong>{message.sender}:</strong> {message.text}
+          </li>
         ))}
-      </div>
-      <form onSubmit={handleSendMessage}>
-        <input
-          type="text"
-          value={newMessage}
-          onChange={handleNewMessageChange}
-          placeholder="Type your message..."
-        />
-        <button type="submit">Send</button>
-      </form>
+      </ul>
     </div>
   );
 }
