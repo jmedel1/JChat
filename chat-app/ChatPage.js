39aa0935e9d9b44d5be469694bbeb36d3df657a5
jael <jael.medel30@gmail.com>
2023-06-28 11:48:47 -0500

    display messages & send

--- chat-app/ChatPage.js
+++ chat-app/ChatPage.js
@@ -1,24 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function ChatPage() {
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState('');
 
   useEffect(() => {
-    // Simulating fetching messages from a server or database
-    // Replace this with your own logic to fetch messages
+    // Fetch messages from the backend or an external API
+    // You can replace this with your own logic
     const fetchMessages = async () => {
-      // Simulating an API call delay
-      await new Promise((resolve) => setTimeout(resolve, 1000));
+      try {
+        // Simulating a fetch request delay with setTimeout
+        await new Promise((resolve) => setTimeout(resolve, 1000));
 
-      // Dummy data for example
-      const dummyMessages = [
-        { id: 1, text: 'Hello', user: 'User1' },
-        { id: 2, text: 'How are you?', user: 'User2' },
-        { id: 3, text: 'I am good. How about you?', user: 'User1' },
-      ];
+        // Mock data for demonstration purposes
+        const response = [
+          { id: 1, sender: 'John', content: 'Hello' },
+          { id: 2, sender: 'Jane', content: 'Hi there' },
+        ];
 
-      setMessages(dummyMessages);
+        setMessages(response);
+      } catch (error) {
+        console.log('Error fetching messages:', error);
+      }
     };
 
     fetchMessages();
@@ -34,34 +37,34 @@ function ChatPage() {
     // Create a new message object
     const newMessageObj = {
       id: messages.length + 1,
-      text: newMessage,
-      user: 'User1', // Replace with the current user or dynamic user
+      sender: 'You', // You can replace this with the user's display name or any other identifier
+      content: newMessage,
     };
 
-    // Update the messages state with the new message
+    // Add the new message to the existing messages array
     setMessages([...messages, newMessageObj]);
 
-    // Clear the newMessage state for the next message
+    // Clear the input field
     setNewMessage('');
   };
 
   return (
     <div>
       <h1>Chat Page</h1>
-      <ul>
+      <div className="message-list">
         {messages.map((message) => (
-          <li key={message.id}>
-            <strong>{message.user}: </strong>
-            {message.text}
-          </li>
+          <div key={message.id} className="message">
+            <strong>{message.sender}: </strong>
+            <span>{message.content}</span>
+          </div>
         ))}
-      </ul>
+      </div>
       <form onSubmit={handleSendMessage}>
         <input
           type="text"
           value={newMessage}
           onChange={handleNewMessageChange}
-          placeholder="Type your message"
+          placeholder="Type your message..."
         />
         <button type="submit">Send</button>
       </form>
