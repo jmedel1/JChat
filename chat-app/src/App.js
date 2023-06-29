645c150fba684e9e92a5e2b629f5d38f95d3fe20
jael <jael.medel30@gmail.com>
2023-06-28 19:57:13 -0500

    import link

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import ChatPage from './ChatPage';
 import SettingsPage from './SettingsPage';
 
