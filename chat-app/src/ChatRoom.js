770aa9617fcd598da862b33386ef9703d3cc60c8
jael <jael.medel30@gmail.com>
2023-07-01 15:38:29 -0500

    clear comment bar

--- chat-app/src/ChatRoom.js
+++ chat-app/src/ChatRoom.js
@@ -4,14 +4,15 @@ const ChatRoom = ({ messages }) => {
   const [comments, setComments] = useState({});
   const [likes, setLikes] = useState({});
   const [dislikes, setDislikes] = useState({});
+  const [comment, setComment] = useState('');
 
-  const handleComment = (messageId, comment) => {
-    const newComment = comment.trim();
-    if (newComment) {
+  const handleComment = (messageId) => {
+    if (comment.trim()) {
       setComments((prevComments) => ({
         ...prevComments,
-        [messageId]: [...(prevComments[messageId] || []), newComment]
+        [messageId]: [...(prevComments[messageId] || []), comment]
       }));
+      setComment(''); // Clear the comment state
     }
   };
 
@@ -32,49 +33,42 @@ const ChatRoom = ({ messages }) => {
   return (
     <div>
       <h2>Chat Room</h2>
-      {messages.map((message) => (
-        <div key={message.id}>
-          <strong>{message.author}: </strong>
-          {message.content}
-          <br />
-          <button onClick={() => handleLike(message.id)}>Like</button>
-          <button onClick={() => handleDislike(message.id)}>Dislike</button>
-          <br />
-          <input
-            type="text"
-            placeholder="Leave a comment..."
-            value={comments[message.id] || ''}
-            onChange={(e) => {
-              const comment = e.target.value;
-              setComments((prevComments) => ({
-                ...prevComments,
-                [message.id]: comment
-              }));
-            }}
-          />
-          <button
-            onClick={() => handleComment(message.id, comments[message.id])}
-          >
-            Comment
-          </button>
-          {comments[message.id] && (
+      {messages.map((message) => {
+        const messageComments = comments[message.id] || [];
+        return (
+          <div key={message.id}>
+            <strong>{message.author}: </strong>
+            {message.content}
+            <br />
+            <button onClick={() => handleLike(message.id)}>Like</button>
+            <button onClick={() => handleDislike(message.id)}>Dislike</button>
+            <br />
+            <input
+              type="text"
+              placeholder="Leave a comment..."
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+            />
+            <button onClick={() => handleComment(message.id)}>Comment</button>
+            {messageComments.length > 0 && (
+              <div>
+                <strong>Comments:</strong>
+                {messageComments.map((comment, index) => (
+                  <div key={index}>{comment}</div>
+                ))}
+              </div>
+            )}
             <div>
-              <strong>Comments:</strong>
-              {comments[message.id].map((comment, index) => (
-                <div key={index}>{comment}</div>
-              ))}
+              <strong>Likes: </strong>
+              {likes[message.id] || 0}
             </div>
-          )}
-          <div>
-            <strong>Likes: </strong>
-            {likes[message.id] || 0}
+            <div>
+              <strong>Dislikes: </strong>
+              {dislikes[message.id] || 0}
+            </div>
           </div>
-          <div>
-            <strong>Dislikes: </strong>
-            {dislikes[message.id] || 0}
-          </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 };
