4bb52e069ab01de68cbe9ff82b789c6de74c4792
jael <jael.medel30@gmail.com>
2023-07-01 15:01:14 -0500

    adding likes and dislikes

--- chat-app/src/ChatRoom.js
+++ chat-app/src/ChatRoom.js
@@ -1,6 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ChatRoom = ({ messages }) => {
+  const [comments, setComments] = useState({});
+  const [likes, setLikes] = useState({});
+  const [dislikes, setDislikes] = useState({});
+
+  const handleComment = (messageId, comment) => {
+    setComments((prevComments) => ({
+      ...prevComments,
+      [messageId]: [...(prevComments[messageId] || []), comment]
+    }));
+  };
+
+  const handleLike = (messageId) => {
+    setLikes((prevLikes) => ({
+      ...prevLikes,
+      [messageId]: (prevLikes[messageId] || 0) + 1
+    }));
+  };
+
+  const handleDislike = (messageId) => {
+    setDislikes((prevDislikes) => ({
+      ...prevDislikes,
+      [messageId]: (prevDislikes[messageId] || 0) + 1
+    }));
+  };
+
   return (
     <div>
       <h2>Chat Room</h2>
@@ -8,6 +33,41 @@ const ChatRoom = ({ messages }) => {
         <div key={message.id}>
           <strong>{message.author}: </strong>
           {message.content}
+          <br />
+          <button onClick={() => handleLike(message.id)}>Like</button>
+          <button onClick={() => handleDislike(message.id)}>Dislike</button>
+          <br />
+          <input
+            type="text"
+            placeholder="Leave a comment..."
+            onChange={(e) => {
+              setComments((prevComments) => ({
+                ...prevComments,
+                [message.id]: [...(prevComments[message.id] || []), e.target.value]
+              }));
+            }}
+          />
+          <button
+            onClick={() => handleComment(message.id, comments[message.id])}
+          >
+            Comment
+          </button>
+          {comments[message.id] && (
+            <div>
+              <strong>Comments:</strong>
+              {comments[message.id].map((comment, index) => (
+                <div key={index}>{comment}</div>
+              ))}
+            </div>
+          )}
+          <div>
+            <strong>Likes: </strong>
+            {likes[message.id] || 0}
+          </div>
+          <div>
+            <strong>Dislikes: </strong>
+            {dislikes[message.id] || 0}
+          </div>
         </div>
       ))}
     </div>
